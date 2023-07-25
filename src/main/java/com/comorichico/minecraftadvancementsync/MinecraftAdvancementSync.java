df535b78fd9637fbfe725ae373a3c8c7217596d6
comorichico <chico@comorichico.com>
2023-07-25 10:40:36 +0900

    init

---
+++ src/main/java/com/comorichico/minecraftadvancementsync/MinecraftAdvancementSync.java
@@ -0,0 +1,17 @@
+package com.comorichico.minecraftadvancementsync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftAdvancementSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
