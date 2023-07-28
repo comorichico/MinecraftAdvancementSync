8a3a802767023228d4a15e0ddbe6ae3d8fe5a0c6
comorichico <chico@comorichico.com>
2023-07-28 23:07:38 +0900

    たぶんだいたい完成してる

--- src/main/java/com/comorichico/minecraftadvancementsync/MinecraftAdvancementSync.java
+++ src/main/java/com/comorichico/minecraftadvancementsync/MinecraftAdvancementSync.java
@@ -11,8 +11,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 public final class MinecraftAdvancementSync extends JavaPlugin implements Listener {
 
@@ -21,10 +23,53 @@ public final class MinecraftAdvancementSync extends JavaPlugin implements Listen
     public void onEnable() {
         // データベース接続などの初期化は非同期で行う
         Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            CreateEnvFile();
             databaseAccess = new DatabaseAccess(this);
         });
+
         // イベントリスナーを登録
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    // .envファイルが存在しない場合に作成する
+    public void CreateEnvFile(){
+
+        File dataFolder = getDataFolder();
+        String absolutePath = dataFolder.getAbsolutePath();
+
+        // ディレクトリが存在しない場合に作成
+        File directory = new File(absolutePath);
+
+        if (!directory.exists()) {
+            boolean created = directory.mkdirs();
+            if (created) {
+                getLogger().log(Level.INFO,"ディレクトリが作成されました。");
+            } else {
+                getLogger().log(Level.INFO,"ディレクトリの作成に失敗しました。");
+            }
+        }
+
+        // .envファイルの絶対パス
+        String envFilePath = absolutePath + File.separator + ".env";
+
+        // .envファイルが存在する場合は終了
+        File envFile = new File(envFilePath);
+        if (envFile.exists()) {
+            getLogger().log(Level.INFO,".envファイルは既に存在します。");
+            return;
+        }
+
+        // .envファイルを作成し、内容を書き込む
+        try {
+            FileWriter writer = new FileWriter(envFile);
+            writer.write("server=domain\n");
+            writer.write("username=user\n");
+            writer.write("password=password\n");
+            writer.close();
+            getLogger().log(Level.INFO,".envファイルが作成されました。");
+        } catch (IOException e) {
+            getLogger().log(Level.INFO,"エラーが発生しました: " + e.getMessage());
+        }
     }
 
     @Override
@@ -69,7 +114,7 @@ public final class MinecraftAdvancementSync extends JavaPlugin implements Listen
     }
 
     @EventHandler
-    public void onPlayerAdvancementCriterionGrant(PlayerAdvancementCriterionGrantEvent event) throws IOException {
+    public void onPlayerAdvancementCriterionGrant(PlayerAdvancementCriterionGrantEvent event) {
         databaseAccess.onPlayerAdvancementCriterionGrant(event);
     }
 }
